c4671844b5ec261774771085f3ca1a1e79d54a16
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2018-03-27 13:10:04 +0200

    Added initial implementation of maybe, added ncrunch support

---
+++ Dys.Tests/MaybeTests.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+using NUnit.Framework;
+
+namespace Dys.Tests
+{
+    [TestFixture]
+    public class MaybeTests
+    {
+        [Test]
+        public void Constructor_Default_ReturnsMaybeWithHasValueFalse()
+        {
+            var maybe = new Maybe<int>();
+            
+            Assert.That(maybe.HasValue, Is.False);
+        }
+
+        [Test]
+        public void Constructor_WithValue_ReturnsMeybeWithHasValueTrue()
+        {
+            var maybe = new Maybe<int>(42);
+
+            Assert.That(maybe.HasValue, Is.True);
+        }
+    }
+}
---
+++ Dys/Maybe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dys
+{
+    public struct Maybe<T>
+    {
+        private readonly bool _HasValue;
+
+        public Maybe(T value)
+        {
+            _HasValue = true;
+        }
+
+        public bool HasValue
+            => _HasValue;
+    }
+}
